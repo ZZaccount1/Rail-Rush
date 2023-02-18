2cdc6e5e7eb49503fff68852b073f152c9a40bc5
ZZ <nmio45137@gmail.com>
2023-02-18 18:18:46 +0200

    Enable all modules

--- conf.lua
+++ conf.lua
@@ -19,10 +19,11 @@ function love.conf(t)
     t.window.highdpi = false           -- Enable high-dpi mode for the window on a Retina display (boolean). Added in 0.9.1
     t.window.srgb = false              -- Enable sRGB gamma correction when drawing to the screen (boolean). Added in 0.9.1
 
+    --[[
     t.modules.audio = false            -- Enable the audio module (boolean)
     t.modules.event = true             -- Enable the event module (boolean)
     t.modules.graphics = true          -- Enable the graphics module (boolean)
-    t.modules.image = false            -- Enable the image module (boolean)
+    t.modules.image = true             -- Enable the image module (boolean)
     t.modules.joystick = false         -- Enable the joystick module (boolean)
     t.modules.keyboard = true          -- Enable the keyboard module (boolean)
     t.modules.math = false             -- Enable the math module (boolean)
@@ -32,4 +33,5 @@ function love.conf(t)
     t.modules.system = false           -- Enable the system module (boolean)
     t.modules.timer = true             -- Enable the timer module (boolean)
     t.modules.window = true            -- Enable the window module (boolean)
+    ]]
 end
