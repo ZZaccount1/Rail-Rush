ca0faab03666c9dd3762b56cca2a10c1e2b5caaa
ZZ <nmio45137@gmail.com>
2023-02-19 02:24:28 +0200

    Fix up and down animation
    The idle animation was playing if the player was moving up or down, but
    now they move depending on the direction the player is facing

--- src/player.lua
+++ src/player.lua
@@ -32,9 +32,21 @@ function player:update(dt)
 
     if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
         player.y = player.y - player.speed
+        
+        if player.facingRight then
+            player.anim = player.animations.right
+        else
+            player.anim = player.animations.left
+        end 
     end
     if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
         player.y = player.y + player.speed
+
+        if player.facingRight then
+            player.anim = player.animations.right
+        else
+            player.anim = player.animations.left
+        end
     end
     if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
         player.x = player.x + player.speed
